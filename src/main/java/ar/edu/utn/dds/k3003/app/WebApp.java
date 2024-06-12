5e944f67290664d85c70d70f7b24d06af3243ce5
fedehartwig <johndoe@example.com>
2024-06-12 16:27:07 -0300

    a

--- src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
+++ src/main/java/ar/edu/utn/dds/k3003/app/WebApp.java
@@ -49,12 +49,16 @@ public class WebApp {
 
     public static ObjectMapper createObjectMapper() {
         var objectMapper = new ObjectMapper();
+        configureObjectMapper(objectMapper);
+        return objectMapper;
+    }
+
+    public static void configureObjectMapper(ObjectMapper objectMapper) {
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         var sdf = new SimpleDateFormat(Constants.DEFAULT_SERIALIZATION_FORMAT, Locale.getDefault());
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
         objectMapper.setDateFormat(sdf);
-        return objectMapper;
     }
 }
